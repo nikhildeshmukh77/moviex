e80d97cceeefa24176343539e5f6fc558eecd8ab
Nikhil Deshmukh <nikhil77d@gmail.com>
2024-06-18 08:46:08 +0530

    Update Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,25 +22,31 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    Discover your next favorite film with Moviex, your ultimate destination for personalized 
+                    movie recommendations and reviews. Connect with us on social media to stay updated on the 
+                    latest releases and hidden gems. Have questions or need support? Reach out to us anytime at
+                     support@Moviex.com. Dive into the world of cinema with confidence and enjoyment!
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
-                        <FaFacebookF />
+                        <a href="https://facebook.com/" target="_blank">
+                            <FaFacebookF />
+                        </a>
                     </span>
                     <span className="icon">
-                        <FaInstagram />
+                        <a href="https://instagram.com/nikhil_20d" target="_blank">
+                            <FaInstagram />
+                        </a>
                     </span>
                     <span className="icon">
-                        <FaTwitter />
+                        <a href="https://twitter.com/NikhilD52316330" target="_blank">
+                            <FaTwitter />
+                        </a>
                     </span>
                     <span className="icon">
-                        <FaLinkedin />
+                        <a href="https://www.linkedin.com/in/nikhildeshmukh22/" target="_blank">
+                            <FaLinkedin />
+                        </a>
                     </span>
                 </div>
             </ContentWrapper>
